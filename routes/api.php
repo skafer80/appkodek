72c1e54877266c90c0d2bbfe12397267b6d6475f
skafer80 <skafer@gmail.com>
2026-03-30 15:35:28 +0200

    aggiorna

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ElencoAllieviClasseController;
 use App\Http\Controllers\Api\ElencoModuliClasseController;
 use App\Http\Controllers\Api\ComuneController;
+use App\Http\Controllers\Api\ModuloConoscenzaController;
 
 
 
@@ -13,3 +14,9 @@ Route::get('/elenco-allievi-classe/{classeId}', [ElencoAllieviClasseController::
 Route::get('/elenco-moduli-classe/{classeId}', [ElencoModuliClasseController::class, 'execute']);
 
 Route::get('/comuni', [ComuneController::class, 'getComuniByProvincia'])->name('api.comuni');
+
+Route::get('/modulo-conoscenza/{id}', [ModuloConoscenzaController::class, 'show'])->name('api.modulo_conoscenza.show');
+
+Route::post('/modulo-conoscenza', [ModuloConoscenzaController::class, 'store']);
+
+Route::post('/moduloother', [ModuloConoscenzaController::class, 'storeOther']);
