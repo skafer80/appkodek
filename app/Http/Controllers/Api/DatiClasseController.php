a8357685f519ea15ef86eb4b306508826de1618f
skafer80 <skafer@gmail.com>
2026-04-14 10:00:50 +0200

    ooo

--- app/Http/Controllers/Api/DatiClasseController.php
+++ app/Http/Controllers/Api/DatiClasseController.php
@@ -4,33 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class DatiClasseController extends Controller
 {
     public function execute(Request $request)
     {
         $classeId = $request->classeId;
+        $response = Http::get('https://www.kodek.it/api/dati-classe/' . $classeId);
 
-        $datiClasse = [
-            [
-                "classeId" => $classeId,
-                "DataAvvio" => "30/11/2025",
-                "DataFine" => "30/11/2025",
-                "Importo" => 1747744,
-                "NumeroGiorniPrevisti" => 30,
-                "DataAvvioStage" => "30/12/2025",
-                "DataFineStage" => "30/12/2025",
-                "NumeroGiorniStage" => 10,
-                "DenominazioneStage" => "cacaacacacacacacac",
-                "PIVA" => "12345678901",
-                "NumeroAllievi" => 10,
-                "Provincia" => "PA",
-                "Comune" => "Palermo",
-                "Indirizzo" => "Via Roma",
-                "NumeroCivico" => "1"
-            ]
-        ];
-
-        return response()->json($datiClasse);
+        return response()->json($response->json());
     }
 }
