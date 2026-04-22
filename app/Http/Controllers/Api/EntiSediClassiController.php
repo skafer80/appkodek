eac6e48fce2700937f7afd1e6688953e61b1f724
skafer80 <skafer@gmail.com>
2026-04-22 11:05:37 +0200

    agg

--- app/Http/Controllers/Api/EntiSediClassiController.php
+++ app/Http/Controllers/Api/EntiSediClassiController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 
 class EntiSediClassiController extends Controller
 {
     public function execute(Request $request)
     {
+        Log::info('API enti-sedi-classi request', [
+            'ip' => $request->ip(),
+            'user_agent' => $request->userAgent(),
+        ]);
+
         $classeId = $request->classeId;
         $response = Http::get('https://kodek.it/api/enti-sedi-classi');
 
