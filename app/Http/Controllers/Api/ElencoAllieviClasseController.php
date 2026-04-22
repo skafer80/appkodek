1f707adbaf689df9007cf1af247e7c21eabe9a0c
skafer80 <skafer@gmail.com>
2026-04-22 11:22:50 +0200

    log

--- app/Http/Controllers/Api/ElencoAllieviClasseController.php
+++ app/Http/Controllers/Api/ElencoAllieviClasseController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 
 class ElencoAllieviClasseController extends Controller
 {
     public function execute(Request $request)
     {
+        Log::info('API elenco-allievi-classe request', [
+            'ip' => $request->ip(),
+            'user_agent' => $request->userAgent(),
+            'classeId' => $request->classeId,
+        ]);
         $classeId = $request->classeId;
         $response = Http::get('https://www.kodek.it/api/elenco-allievi-classe/' . $classeId);
 
