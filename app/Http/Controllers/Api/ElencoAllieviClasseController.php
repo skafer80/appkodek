bb22d309974556fd4fe4baedd21a1cfc8eb5e6b0
skafer80 <skafer@gmail.com>
2026-04-22 15:53:41 +0200

    agg

--- app/Http/Controllers/Api/ElencoAllieviClasseController.php
+++ app/Http/Controllers/Api/ElencoAllieviClasseController.php
@@ -7,18 +7,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
-
 class ElencoAllieviClasseController extends Controller
 {
     public function execute(Request $request)
     {
-        Log::info('API elenco-allievi-classe request', [
-            'ip' => $request->ip(),
-            'user_agent' => $request->userAgent(),
-            'classeId' => $request->classeId,
-        ]);
+        if (! in_array($request->ip(), ['93.95.216.134', '93.95.216.134'])) {
+            Log::info('API elenco-allievi-classe request', [
+                'ip' => $request->ip(),
+                'user_agent' => $request->userAgent(),
+                'classeId' => $request->classeId,
+            ]);
+        }
         $classeId = $request->classeId;
-        $response = Http::get('https://www.kodek.it/api/elenco-allievi-classe/' . $classeId);
+        $response = Http::get('https://www.kodek.it/api/elenco-allievi-classe/'.$classeId);
 
         return response()->json($response->json());
     }
--- app/Http/Controllers/Api/EntiSediClassiController.php
+++ app/Http/Controllers/Api/EntiSediClassiController.php
@@ -12,10 +12,12 @@ class EntiSediClassiController extends Controller
 {
     public function execute(Request $request)
     {
-        Log::info('API enti-sedi-classi request', [
-            'ip' => $request->ip(),
-            'user_agent' => $request->userAgent(),
-        ]);
+        if (!in_array($request->ip(), ['93.95.216.134', '93.95.216.134'])) {
+            Log::info('API enti-sedi-classi request', [
+                'ip' => $request->ip(),
+                'user_agent' => $request->userAgent(),
+            ]);
+        }
 
         $classeId = $request->classeId;
         $response = Http::get('https://kodek.it/api/enti-sedi-classi');
